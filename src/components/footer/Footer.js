321f42895a9fc613adfeff47024f4dc472cc5335
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-10-05 21:02:47 +0530

    with header and footer added other pages are remaing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>This is a Footer Page</div>
-  )
+    <div>
+        <div className='footer'>
+          {/* useful links */}
+          <div className='usefule-link'>
+            <h3>Usefule Links</h3>
+            <p>Home</p>
+            <p>Contact</p>
+            <p>About</p>
+          </div>
+            {/*Address  */}
+          <div className='address'>
+            <h3>Carporate Office</h3>
+            <p>Kothrud</p>
+            <p>Pune 411038</p>
+          </div>
+
+    </div>
+  </div>
+    
+    )
 }
 
 export default Footer
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>This is a Navbar</div>
+    
+    <div className='nav'>
+      <div className='navTitle'>Resume <span>Builder</span></div>
+      <div className='navLink'>
+        <span><Link className='link' to='/'>Home</Link></span>
+        <span><Link className='link' to='/login'>Login</Link></span>
+        <span><Link className='link' to='/register'>Register</Link></span>
+        <span><Link className='link' to='/resume'>Resume</Link></span>
+
+      </div>  
+    </div>
   )
 }
 
